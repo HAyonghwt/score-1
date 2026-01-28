b8ff9b0c7ec16f3fc583340a6853f6730a381abe
HAyonghwt <dp30@naver.com>
2026-01-28 13:12:16 +0900

    구글 AP키 공개된것 보안을 보완함

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getAnalytics, isSupported as isAnalyticsSupported } from "firebase/anal
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCk9ha_dN1LE9_3tQwME_yHxwBwwGOHlaU",
-  authDomain: "contestdate.firebaseapp.com",
-  databaseURL: "https://contestdate-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "contestdate",
-  storageBucket: "contestdate.firebasestorage.app",
-  messagingSenderId: "138458164588",
-  appId: "1:138458164588:web:90525ed13159e5f512d749",
-  measurementId: "G-VNH22LC97E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase (SSR-safe)
